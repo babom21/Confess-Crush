cf591928d764e1d251060b0857808d0eeaadd2ed
babom21 <118040988+babom21@users.noreply.github.com>
2022-11-12 15:06:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘',
+    messDesc: 'Được rồi, về nhắn tin với tớ đi.',
+    btnAccept: 'Oci',
+    messLink: 'https://www.facebook.com/profile.php?id=100034297382052' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
